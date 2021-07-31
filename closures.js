a2db2344d71cfb051cd5325a6757af58665ce4f9
Nasium Hill <zainullinaaliya45@gmail.com>
2021-07-31 16:17:08 +0300

    closures

--- closures.js
+++ closures.js
@@ -1,11 +1,43 @@
-function sayHelloTo(name) {
-	const message = 'Hello' + name
-	return function () {
-		console.log(message)
-	}
+// function sayHelloTo(name) {
+// 	const message = 'Hello' + name
+// 	return function () {
+// 		console.log(message)
+// 	}
+// }
+//
+// const HelloToAliya = sayHelloTo ('Aliya')
+// console.log(HelloToAliya)
+// console.log(HelloToAliya())
+// HelloToAliya()
+
+// function createFw() {
+// 	const fw = ['Vue', 'React']
+//
+// 	return {
+// 		print: function () {
+// 			console.log(fw.join(' '))
+// 		},
+// 		add: function (framework) {
+// 			fw.push(framework)
+// 		}
+// 	}
+// }
+//
+// const manager = createFw()
+// console.log(manager)
+// manager.print()
+// manager.add('Angular')
+// manager.print()
+
+
+// setTimeout
+
+const fib = [1, 2, 3, 5, 8, 13]
+
+for (var i = 0; i < fib.length; i++)
+{
+	setTimeout(function () {
+		console.log(`fib[${i}] = ${fib[i]}`)
+	}, 1500)
 }
 
-const HelloToAliya = sayHelloTo ('Aliya')
-console.log(HelloToAliya)
-console.log(HelloToAliya())
-HelloToAliya()
