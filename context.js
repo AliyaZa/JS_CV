cabf3cb8f1602dba89545f2e663fce7d5ab4bbad
Nasium Hill <zainullinaaliya45@gmail.com>
2021-08-01 16:15:12 +0300

    context

---
+++ context.js
@@ -0,0 +1,11 @@
+const person = {
+	surname: 'Stark'
+	knows: function (what, name) {
+		console.log(`you ${what} know, ${name} ${this.surname}`)
+	}
+}
+
+const john = { surname: 'Snow'}
+
+person.knows ('all', 'Bran')
+person.knows.call(john, 'nothing', 'John')
