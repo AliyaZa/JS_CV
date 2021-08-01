9ed6136829ac279c88cf18cfd066ccf093c04417
Nasium Hill <zainullinaaliya45@gmail.com>
2021-08-01 16:06:02 +0300

    iife

--- iife.js
+++ iife.js
@@ -8,3 +8,14 @@ for (var i = 0; i < 5; i++) {
 }
 result[2]
 result[4]
+
+for (var i = 0; i < 5; i++)
+ {(
+	 function() {
+		 var j = i
+		 result.push( function() {console.log(j)})
+	 })()
+ }
+
+ result[2]()
+ result[4]()
